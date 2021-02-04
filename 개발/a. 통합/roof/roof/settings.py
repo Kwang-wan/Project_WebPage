bc8066163235033041a236582656755b8029f9ff
Kwang-wan Woo <62735349+Kwang-wan@users.noreply.github.com>
2021-02-04 17:57:24 +0900

    20210204 mod

--- 개발/a. 통합/roof/roof/settings.py
+++ 개발/a. 통합/roof/roof/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_p@sepgn25yi*4-b^f91y78r0fovda+m^%8zyzy7b%#$z4(p*z'
+SECRET_KEY = BASE_DIR / 'secret.json'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
